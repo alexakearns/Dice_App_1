05f708dc3ad4b5e24da1dfdf5a46554cb4d33a6c
Alexa <alexakearns@outlook.com>
2020-05-16 18:34:08 +0100

    game rolls any number of dice in one go - Red

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,11 @@ describe Game do
     3.times { subject.new_dice }
     expect(subject.all_dice.length).to eq 3
   end
+
+  it "roll's any number of dice in one go" do
+    10.times { subject.new_dice }
+    subject.roll_dice(4)
+    expect(subject.dice_result).to contain(be_a_kind_of(Integer))
+    expect(subject.dice_result.length).to eq 4
+  end
 end
